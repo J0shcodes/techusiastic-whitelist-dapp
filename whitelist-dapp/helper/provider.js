7dc14478527ce8d7376a98c9b861be32528d5ac1
J0shcodes <mosesjoshua350@gmail.com>
2023-04-19 05:33:34 +0100

    Important push

--- whitelist-dapp/helper/provider.js
+++ whitelist-dapp/helper/provider.js
@@ -2,10 +2,11 @@ import { providers } from "ethers";
 
 const getProvider = async (web3ModalRef, needSigner = false) => {
   const provider = await web3ModalRef.current.connect();
+  console.log(provider)
   const web3Provider = new providers.Web3Provider(provider);
 
   const { chainId } = await web3Provider.getNetwork();
-  if(chainId !== 4) {
+  if(chainId !== 5) {
     window.alert("Change network to Rinkeby");
     throw new Error("Change network to Rinkeby");
   }
